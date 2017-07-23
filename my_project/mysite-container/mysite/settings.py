75d1cede811b8644893f937ba8e2418d04ba89e8
nkg5096 <nkg5096@psu.edu>
2017-07-23 13:34:20 -0400

    Django

--- my_project/mysite-container/mysite/settings.py
+++ my_project/mysite-container/mysite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8(_9dce2cbu*ww(^+9-l$2b^lvl09$zr)i3$)17q8pak*2%l05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
